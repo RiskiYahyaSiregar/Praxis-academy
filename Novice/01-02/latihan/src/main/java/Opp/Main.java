29d62aa6390167f2f566928217a8427ea5e41b4a
Yahya <farisuwais46@gmail.com>
2020-03-05 16:40:20 +0700

    Hari Ketiga

---
+++ Novice/01-02/latihan/src/main/java/Opp/Main.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+
+package belajar;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] arg){
+
+        boolean kondisi = true;
+    do{
+
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.print("Masukkan Nama Pembeli : ");
+        String namaPembeli = inputScanner.nextLine();
+
+        System.out.print("Masukkan Alamat Pembeli");
+        String alamatPembeli = inputScanner.nextLine();
+
+        Customer customer1 = new Customer(namaPembeli, alamatPembeli);
+
+        System.out.print("Masukkan Nama Barang");
+        String namaBarang = inputScanner.nextLine();
+
+        System.out.print("Masukkan Harga Barang");
+        int hargaBarang = inputScanner.nextLine();
+
+        System.out.print("Masukkan Jumlah Barang");
+        int jumlahBarang = inputScanner.nextInt();
+
+        Order order1 = new Order(namaBarang, hargaBarang, jumlahBarang);
+        customer1.order(order1);
+
+
+        System.out.print("Lanjut ? (Y/T) : ");
+        String kondisiInput = inputScanner.next();
+
+        if (kondisiInput.equalsIgnoreCase("y"));{
+            kondisi = true;
+        
+        }else{
+            kondisi = false;
+      }
+}while(kondisi);
+
+    
+}
+class Customer{
+    public class Customer {
+        private String place_order;
+        private String know_name;
+        private String know_address;
+        private String know_castomer;
+        private String order_history;
+    
+        public Customer(String know_name, String know_address){
+            this.know_name = know_name;
+            this.know_address = know_address;
+        }
+        public void order(Order order){
+            order.orderItemDetail();
+        }
+        class Order{
+    
+    private String Know_placement_date;
+    private String delivery_date;
+    private String order_items;
+    private String amount;
+    private int total;
+    private int price_items;
+    private String applicable_taxes;
+    private String order_number;
+
+    public Order(String order_items, int price_items, int amount){
+        this.order_items = order_items;
+        this.price_items = price_items;
+        this.amount = amount;
+
+    }
+    public void orderItemsDetail(){
+        total = price_items * amount;
+
+        System.out.println("--------- your detail order------");
+        System.out.println("name item : " + order_items);
+        System.out.println("price item : " + price_items);
+        System.out.println("amount item : " + amount);
+        System.out.println("total : " + total);
+        
+    }
+}   
---
+++ Novice/01-02/latihan/src/main/java/contohkasus/Customer.java
@@ -0,0 +1,23 @@
+package contohkasus;
+
+
+/**
+* custumer
+*/
+public class Customer {
+    private String place_order;
+    private String know_name;
+    private String know_address;
+    private String know_castomer;
+    private String order_history;
+
+    public Customer(String know_name, String know_address){
+        this.know_name = know_name;
+        this.know_address = know_address;
+
+    }
+    public void order(Order order){
+        order.orderItemsDetail();
+        
+    }
+}
